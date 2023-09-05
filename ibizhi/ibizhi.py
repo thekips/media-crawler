e1c6a264703268ed59602a401a2ce2d25f478c7c
thekips <issizhe@gmail.com>
2023-09-05 12:49:35 +0800

    add classify func for ibizhi

--- ibizhi/ibizhi.py
+++ ibizhi/ibizhi.py
@@ -4,13 +4,50 @@ import os
 import re
 import requests
 import yaml
+from concurrent.futures import ThreadPoolExecutor
 
 # Init for decode
 URL_CLIENT = 'https://client.ibzhi.com/http/client'
 CryptoJS = js2py.require('crypto-js')
+CLASS = ['wallpaper/get_wallpaper_list', 'wallpaper/get_list_by_classify']
+PATH_DICT = {-1:'最近壁纸', 0: '电脑壁纸', 1: '静物'}
+CLASSIFY_ID = ['5fb341d2d1cb4d00014a38dd', '5fac0f5e8404d300019eeb5f']
+FUNC = [
+    lambda page:'{"page":%s,"size":30,"v":3}' % str(page),
+    lambda page, c_id:'{"page":%s,"size":30,"v":3,"classify_ids":"%s"}' % (str(page), c_id),
+]
+
+# Request params
+def get_params(page, f_index, c_index=None):
+    if page == None: page = 1e16
+
+    params = {
+        "token": token,
+        "path": CLASS[f_index],
+    }
+    if f_index == 0:
+        params['param'] = FUNC[f_index](page)
+    if f_index == 1:
+        params['param'] = FUNC[f_index](page, CLASSIFY_ID[c_index])
+
+    # print(params)
+    return params
+
+# Initial args.
+f_index = int(input('输入 0.获取最近壁纸 1.获取类别壁纸 :'))
+if f_index == 0:
+    params = lambda page=None : get_params(page, f_index)
+    PATH = 'data/' + PATH_DICT[-1]
+elif f_index == 1:
+    c_index = int(input(f'输入 {PATH_DICT} :'))
+    params = lambda page=None : get_params(page, f_index, c_index)
+    PATH = 'data/' + PATH_DICT[c_index]
+
 with open('config.yaml', 'r', encoding='utf-8') as f:
     token = yaml.safe_load(f)['token']
 
+
+# Methods.
 def mkDir(path):
     if(not os.path.exists(path)):
         try:
@@ -25,19 +62,11 @@ def decode_b64_aes(enc):
     dec = CryptoJS.AES.decrypt(enc, key).toString(CryptoJS.enc.Utf8)
     return eval(dec)
 
-# Request params
-def get_params(page):
-    params = {
-        'token': token,
-        'param': '{"page":%s,"size":30,"v":3}' % str(page),
-        'path': 'wallpaper/get_wallpaper_list',
-    }
-    return params
 
-def dl_pic(url, tag):
+def dl_pic(img_url, tag):
     img_name = re.findall(r'(?<=wallpaper/).*?(?=\?x-image)', img_url)[0]
-    path = f'data/{tag}/'
-    img_path = path + img_name
+    path = os.path.join(PATH, tag)
+    img_path = os.path.join(path, img_name)
 
     try:
         if not os.path.exists(img_path):
@@ -54,25 +83,41 @@ def dl_pic(url, tag):
             print(f'Exist {img_url} -> {tag}')
     except:
         print('Try to download the picture again...')
-        dl_pic(url, tag)
+        dl_pic(img_url, tag)
 
-# Get total pages.
-resp = requests.get(URL_CLIENT, params=get_params(1e16))
+
+# Get total pages num.
+resp = requests.get(URL_CLIENT, params=params())
+# print(resp.text)
 info = decode_b64_aes(resp.text)
 total_pages = info['totalPages']
 print(f'Total Pages Num: {total_pages}')
 
+while True:
+    begin_page = input('Input the page num you want to start with: ')
+    if begin_page.isnumeric():
+        begin_page = int(begin_page)
+        break
+    else:
+        print('Please input num without alphabet!')
 
 # Start crawler.
 for page in range(total_pages):
+    if begin_page > page + 1: continue
+
     print(f'=====Downloading Page {page + 1}=====')
-    resp = requests.get(URL_CLIENT, params=get_params(page))
+    resp = requests.get(URL_CLIENT, params=params(page))
+    # print(params())
     wallpaper_list = decode_b64_aes(resp.text)
 
     page_num = len(wallpaper_list['data'])
     print(f'Total Pages Image Num: {page_num}')
 
+    tags = []
+    img_urls = []
     for wallpaper_info in wallpaper_list['data']:
-        tag = wallpaper_info['tag'].encode('latin1').decode('utf-8')
-        img_url = wallpaper_info['originalUrl']
-        dl_pic(img_url, tag)
+        img_urls.append(wallpaper_info['originalUrl'])
+        tags.append(wallpaper_info['tag'].encode('latin1').decode('utf-8'))
+    
+    with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+        executor.map(dl_pic, img_urls, tags)
